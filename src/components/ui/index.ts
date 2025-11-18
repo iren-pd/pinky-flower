af9c8b53f22e8f3a7af1032214ed92210edbaede
iren-pd <irenpd45@gmail.com>
2025-11-18 19:17:25 +0200

    feat(ui): add dropdown and separate comonents

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,4 +3,14 @@ export { Button } from './button';
 export { Input } from './input';
 export { Label } from './label';
 export { Tabs, TabsList, TabsTrigger, TabsContent } from './tabs';
+export { Separator } from './separator';
+export {
+    DropdownMenu,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut
+} from './dropdown-menu';
 export { Toaster } from './sonner';
